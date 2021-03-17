f75a1c7313b46b6ac8f4ae99997f4a75a109faf2
Janith Wanniarachchi <janithcwanni@gmail.com>
2021-03-18 01:03:08 +0530

    minor speed improvements

--- experiments/experiment_1.R
+++ experiments/experiment_1.R
@@ -6,7 +6,7 @@ library(profvis)
 data(M4)
 M4D <- Filter(function(l) l$period == "Daily", M4)
 rm(M4)
-M4D_x <- lapply(M4D, function(temp){temp$x})[100:110]
+M4D_x <- lapply(M4D, function(temp){temp$x})[100:165]
 profvis({
   m4d_f1 <- find_dataleaks(M4D_x, h=14, cutoff = 1,boost=TRUE)
 })
