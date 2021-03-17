0ae0a35b44b59501b0d2d9547d6da571a2ec72ba
Janith Wanniarachchi <janithcwanni@gmail.com>
2021-03-18 01:44:45 +0530

    add tests for corw

--- tests/testthat/test-find_dataleaks.R
+++ tests/testthat/test-find_dataleaks.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("corw detects perfect correlation", {
+  y <- c(2,3,5,6)
+  x <- c(13,14,15,y,24,25,27)
+  expect_equal(round(corw(x,y),4),
+               c(NA,NA,NA,
+                 -0.6332,-0.9191,-0.5815,
+                 1.0,0.8055,0.9640,0.8055))
 })
