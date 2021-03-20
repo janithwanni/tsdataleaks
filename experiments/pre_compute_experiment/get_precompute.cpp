4c6b5e60737dfc24cbceaea76ad964554f6f4973
Janith Wanniarachchi <janithcwanni@gmail.com>
2021-03-20 17:08:16 +0530

    starting on precomputing matrices

---
+++ experiments/pre_compute_experiment/get_precompute.cpp
@@ -0,0 +1,36 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+NumericVector timesTwo(NumericVector x) {
+  return x * 2;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically
+// run after the compilation.
+//
+
+NumericMatrix getSx(List lstx){
+  NumericMatrix m;
+  int N = lstx.length();
+  for(int i = 0;i<N;i++){
+
+  }
+  return m;
+}
+
+/*** R
+timesTwo(42)
+*/
