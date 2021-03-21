50c3dfb5d47f741e8f6a804d50912b168608e2d2
Janith Wanniarachchi <janithcwanni@gmail.com>
2021-03-21 13:54:03 +0530

    finished precomputing correlation values

--- experiments/pre_compute_experiment/get_precompute.cpp
+++ experiments/pre_compute_experiment/get_precompute.cpp
@@ -1,36 +1,34 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
 // [[Rcpp::export]]
-NumericVector timesTwo(NumericVector x) {
-  return x * 2;
+NumericVector getCorr(NumericVector denom_X,NumericMatrix X,NumericVector sX,
+                      int L,int h,NumericVector Ycounter){
+  NumericVector rvec;
+  Ycounter = Ycounter - 1;
+  int prevCounter = 0;
+  for(int i = 0;i<Ycounter.length();i++){
+    int counter = Ycounter[i];
+    for(int Xcounter = prevCounter;Xcounter<counter;Xcounter++){
+      double den_X = (double)denom_X[Xcounter];
+      double den_Y = (double)denom_X[counter];
+      double den = std::sqrt(den_X * den_Y);
+      double num_left = h * sum(X(Xcounter,_) * X(counter,_));
+      double num_right = sX[Xcounter] * sX[counter];
+      double num = num_left - num_right;
+      double r = num / den;
+      rvec.push_back(r);
+    }
+    prevCounter = counter + 1;
+  }
+  return rvec;
 }
 
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
-
-NumericMatrix getSx(List lstx){
-  NumericMatrix m;
-  int N = lstx.length();
-  for(int i = 0;i<N;i++){
-
-  }
-  return m;
+// [[Rcpp::export]]
+double getSum(NumericVector x, NumericVector y){
+  return sum(x * y);
 }
 
 /*** R
-timesTwo(42)
+
 */
--- experiments/pre_compute_experiment/pre_compute.R
+++ experiments/pre_compute_experiment/pre_compute.R
@@ -1,2 +1,48 @@
-Rcpp::sourceCpp('experiments/split_list.cpp')
-Rcpp::sourceCpp('experiments/cpp_rowsums.cpp')
+Rcpp::sourceCpp('experiments/pre_compute_experiment/split_list.cpp')
+# Rcpp::sourceCpp('experiments/pre_compute_experiment/cpp_rowsums.cpp')
+# Rcpp::sourceCpp("experiments/pre_compute_experiment/get_precompute.cpp")
+
+build_endpoints <- function(lstx,h,cutoff){
+  profvis::profvis({
+    n <- length(lstx)
+    lslens <- unlist(lapply(lstx,length))
+    lsnames <- names(lstx)
+    L <- sum(lslens) - (n * h) + n
+    X <- split_list(lstx,h,L) # matrix X (L x h)
+    XsqS <- rowSums(X * X) # sigma(X^2)
+    sX <- rowSums(X)
+    sXsq <- sX * sX # sigma(X)^2
+    denom_X <- (h * XsqS) - sXsq
+
+    Ycounter <- cumsum(lslens - h + 1)
+
+    prevCounter <- 1
+    for(counter in Ycounter){
+      X_vals <- X[prevCounter:counter,]
+      y_val <- X[counter,]
+      sXY <-  rowSums(t(t(X_vals) * y_val))
+      indice <- 1
+      for(Xcounter in prevCounter:counter){
+        denom <- sqrt(denom_X[Xcounter] * denom_X[counter])
+        num_left <- sXY[indice] * h
+        indice <- indice + 1
+        num_right <- sX[Xcounter] * sX[counter]
+        numer <- num_left - num_right
+        r = numer / denom
+      }
+      prevCounter <- counter + 1
+    }
+  })
+}
+
+lst <- list(
+  a = 1:6,
+  b = 7:11
+)
+# build_endpoints(lst,3,1)
+library(M4comp2018)
+data(M4)
+M4D <- Filter(function(l) l$period == "Daily", M4)
+rm(M4)
+M4D_x <- lapply(M4D, function(temp){temp$x})
+build_endpoints(M4D_x,14,1)
